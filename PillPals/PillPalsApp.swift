d9d6c747c36d98f6a701cad5c154a2fdf676d700
atekumulla <tekumull@umich.edu>
2023-11-19 12:40:35 -0500

    Initial commit

---
+++ PillPals/PillPalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PillPalsApp.swift
+//  PillPals
+//
+//  Created by anush on 11/19/23.
+//
+
+import SwiftUI
+
+@main
+struct PillPalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
