7f2493065c6443685d7f62326740a343dfb0f254
aadishiv <aadishiv@umich.edu>
2023-11-28 14:23:23 -0500

    Modified some views, created home view, added some basic notifcation support, foundation of persistence storage

---
+++ PillPals/PersistenceController.swift
@@ -0,0 +1,8 @@
+//
+//  PersistenceController.swift
+//  PillPals
+//
+//  Created by Aadi Shiv Malhotra on 11/22/23.
+//
+
+import Foundation
---
+++ PillPals/Views/NotifcationView.swift
@@ -0,0 +1,8 @@
+//
+//  NotifcationView.swift
+//  PillPals
+//
+//  Created by Aadi Shiv Malhotra on 11/27/23.
+//
+
+import Foundation
