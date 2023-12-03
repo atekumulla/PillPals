ece00e2f0419434c8ed0e6edbe71472a2af5976d
Ramses Mendoza-Hernandez <123204298+ramsesmh0@users.noreply.github.com>
2023-12-03 14:34:58 -0500

    added

--- MedList_var.swift
+++ MedList_var.swift
@@ -6,3 +6,37 @@
 //
 
 import Foundation
+import SwiftUI
+struct Medication: Identifiable {
+
+    var id = UUID() // Unique identifier for each medication
+    var Brand_name: String
+    var Status_color: Color // Could be a string like "Red", "Blue", etc.
+    var priority_color: Color
+    var dosage: Double
+    var image: UIImage
+    var Active: Bool
+}
+
+
+ 
+
+var dummyMedications: [Medication] = [
+    Medication(
+        Brand_name: "Aspirin",
+        Status_color: .red,
+        priority_color: .red,
+        dosage: 12.1,
+        image: UIImage(named: "Aspirin_image")!,
+        Active: true
+    ),
+    Medication(
+        Brand_name: "Ibuprofen",
+        Status_color: .blue,
+        priority_color: .red,
+        dosage: 12.1,
+        image: UIImage(named: "Ibuprofen_image")!,
+        Active: false
+    ),
+    // ... more dummy medications
+]
