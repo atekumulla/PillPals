7ba1b9872989e6bff07082cd30852a06539a445a
tsapre123 <tsapre@umich.edu>
2023-12-03 14:34:36 -0500

    changed color scheme

--- MedList_var.swift
+++ MedList_var.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+import SwiftUI
+struct Medication: Identifiable {
+
+var id = UUID() // Unique identifier for each medication
+var name: String
+var color: String // Could be a string like "Red", "Blue", etc.
+
+var imageName: String = "pills" // Default SF Symbol for a pill
+}
