56fba458d011862ae3854aff1127fd0822adcf1e
aadishiv <aadishiv@outlook.com>
2024-02-11 18:35:25 -0500

    core data start

--- PillPals/Backend/PersistenceController.swift
+++ PillPals/Backend/PersistenceController.swift
@@ -6,7 +6,7 @@
 //
 
 // bringing back
-/*
+
 import Foundation
 import CoreData
 
@@ -17,28 +17,12 @@ struct PersistenceController {
     // Storage for Core Data
     let container: NSPersistentContainer
 
-    // A test configuration for SwiftUI previews
-    static var preview: PersistenceController = {
-        let controller = PersistenceController(inMemory: true)
-
-        // Create 10 example programming languages.
-        for i in 0..<10 {
-            let medicationExample = MedicationModel(context: controller.container.viewContext)
-            medicationExample.id = UUID()
-            medicationExample.name = "Drug" + String(i)
-            medicationExample.type = "Pill"
-            
-        }
-
-        return controller
-    }()
-
     // An initializer to load Core Data, optionally able
     // to use an in-memory store.
     init(inMemory: Bool = false) {
         // If you didn't name your model Main you'll need
         // to change this name below.
-        container = NSPersistentContainer(name: "Main")
+        container = NSPersistentContainer(name: "MedicationModel")
 
         if inMemory {
             container.persistentStoreDescriptions.first?.url = URL(fileURLWithPath: "/dev/null")
@@ -51,4 +35,4 @@ struct PersistenceController {
         }
     }
 }
-*/
+
