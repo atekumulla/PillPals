39db6f81207eaf796b03e9ca20fb54fbcaaacc0a
tsapre123 <tsapre@umich.edu>
2024-03-10 14:17:06 -0400

    calendar heading

--- PillPals/SubViews/CalendarView.swift
+++ PillPals/SubViews/CalendarView.swift
@@ -55,28 +55,35 @@ struct CaregiverCalendarView: View {
     @State private var selectedDate: Date? = nil // Track the selected date
     
     var body: some View {
-        LazyVGrid(columns: columns) {
-            ForEach(0..<7) { index in
-                let date = dateForDay(index: index)
-                
-                VStack { // Use VStack to stack day of week and day of month vertically
-                    Text("\(dayOfWeek(for: date))") // Display day of the week horizontally
-                        .font(.caption)
-                    Button(action: {
-                        selectedDate = date // Set the selected date when clicked
-                    }) {
-                        Text("\(dayOfMonth(for: date))")
-                            .frame(width: 30, height: 30)
-                            .background(
-                                // Highlight light blue if the date is selected
-                                selectedDate == date ? Color.blue.opacity(0.3) : Color.clear
-                            )
-                            .cornerRadius(15)
+        VStack { // Wrap the LazyVGrid inside a VStack
+            Text("Calendar")
+                .font(.title)// Add a heading
+                . padding(.top, 20)
+            
+            LazyVGrid(columns: columns) {
+                ForEach(0..<7) { index in
+                    let date = dateForDay(index: index)
+                    
+                    VStack { // Use VStack to stack day of week and day of month vertically
+                        Text("\(dayOfWeek(for: date))") // Display day of the week horizontally
+                            .font(.caption)
+                        Button(action: {
+                            selectedDate = date // Set the selected date when clicked
+                        }) {
+                            Text("\(dayOfMonth(for: date))")
+                                .frame(width: 30, height: 30)
+                                .background(
+                                    // Highlight light blue if the date is selected
+                                    selectedDate == date ? Color.blue.opacity(0.3) : Color.clear
+                                )
+                                .cornerRadius(15)
+                        }
                     }
                 }
             }
         }
     }
+    
     private func dateForDay(index: Int) -> Date {
         let calendar = Calendar.current
         let startOfWeek = calendar.date(from: calendar.dateComponents([.yearForWeekOfYear, .weekOfYear], from: currentDate))!
@@ -95,6 +102,5 @@ struct CaregiverCalendarView: View {
         return formatter.string(from: date)
     }
 }
-    
 
 
